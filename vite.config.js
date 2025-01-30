5cb6e3c1f01f0cb9b94611448e1351892214dca7
David Oshidero <tayo.os@outlook.com>
2025-01-30 10:55:08 +0000

    Modified ViteConfig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return 'vendor'; // Moves dependencies to a separate file
+                    }
+                },
+            },
+        },
+        chunkSizeWarningLimit: 1000, // Increases the warning limit to 1000 kB
+    },
+});
